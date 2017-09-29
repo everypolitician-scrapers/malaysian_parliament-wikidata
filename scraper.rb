b2a7a597c7258d876df851b4c0cc299da4058005
Tony Bowden <tony@tmtm.com>
2017-09-29 07:14:47 +0100

    Also scrape all people previously matched in EP

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,13 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
+
+existing = EveryPolitician::Index.new.country('Malaysia').lower_house.popolo.persons.map(&:wikidata).compact
 
 en_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/malaysian_parliament-wp', column: 'wikipedia__en') |
            WikiData::Category.new( 'Category:Members of the Dewan Rakyat', 'en').member_titles
 ms_names = WikiData::Category.new( 'Kategori:Ahli Parlimen Malaysia 1959', 'ms').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, ms: ms_names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: en_names, ms: ms_names })
