2a7cab4a318db52cb58fbca2c1f25c0b7665f300
Tony Bowden <tony@tmtm.com>
2017-09-29 07:26:02 +0100

    Change renamed Malaysian category

--- scraper.rb
+++ scraper.rb
@@ -8,6 +8,6 @@ existing = EveryPolitician::Index.new.country('Malaysia').lower_house.popolo.per
 
 en_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/malaysian_parliament-wp', column: 'wikipedia__en') |
            WikiData::Category.new( 'Category:Members of the Dewan Rakyat', 'en').member_titles
-ms_names = WikiData::Category.new( 'Kategori:Ahli Parlimen Malaysia 1959', 'ms').member_titles
+ms_names = WikiData::Category.new( 'Kategori:Ahli Parlimen Malaysia', 'ms').member_titles
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: en_names, ms: ms_names })
