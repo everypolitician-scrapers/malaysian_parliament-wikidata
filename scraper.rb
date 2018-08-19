599d3b53c53566c6062307e4b4b6224c15c8f676
Tony Bowden <tony@tmtm.com>
2018-08-19 16:09:51 +0100

    Don't fetch from other scraper
    The correct entries in that will make it into the `existing` list
    anyway, so no need to make _this_ scraper also depend on that one
    (especially as it's currently broken)

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,7 @@ require 'wikidata/fetcher'
 
 existing = EveryPolitician::Index.new.country('Malaysia').lower_house.popolo.persons.map(&:wikidata).compact
 
-en_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/malaysian_parliament-wp', column: 'wikipedia__en') |
-           WikiData::Category.new( 'Category:Members of the Dewan Rakyat', 'en').member_titles
+en_names = WikiData::Category.new( 'Category:Members of the Dewan Rakyat', 'en').member_titles
 ms_names = WikiData::Category.new( 'Kategori:Ahli Parlimen Malaysia', 'ms').member_titles
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: en_names, ms: ms_names })
