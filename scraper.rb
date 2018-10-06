12ef3a8efd59811ca4a76497b374365d624847c0
Tony Bowden <tony@tmtm.com>
2018-10-06 12:48:44 +0100

    Also find via Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -9,4 +9,7 @@ existing = EveryPolitician::Index.new.country('Malaysia').lower_house.popolo.per
 en_names = WikiData::Category.new( 'Category:Members of the Dewan Rakyat', 'en').member_titles
 ms_names = WikiData::Category.new( 'Kategori:Ahli Parlimen Malaysia', 'ms').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: en_names, ms: ms_names })
+sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39/ps:P39 wd:Q21290861 }'
+p39s = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing | p39s, names: { en: en_names, ms: ms_names })
